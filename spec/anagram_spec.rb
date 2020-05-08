93262f0974bf41ee913bf71e23f0c49e8e212e51
Reid Ashwill <reidashwill@gmail.com>
2020-05-08 09:17:48 -0700

    sucessfully fail third test to check case insenitivity

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -15,3 +15,10 @@ describe("word_validator") do
     expect(set2.word_validator).to(eq(true))
   end
 end
+
+describe("case_insientitive") do
+  it("will determine if the case_insenitive method will treat all words the same regardless of case") do
+    set3 = Anagram.new("Test")
+    expect(set3.word1).to(eq("test"))
+  end
+end    
