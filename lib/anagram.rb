9c2d06b4c799494d9e2bb29d767139be46db26d2
Reid Ashwill <reidashwill@gmail.com>
2020-05-08 08:30:58 -0700

    succesfully fail first test checking if the input type is a string

---
+++ lib/anagram.rb
@@ -0,0 +1,7 @@
+class Anagram
+  attr_accessor(:user_input)
+  
+  def initialize(user_input)
+    @user_input = user_input
+  end
+end    
---
+++ spec/anagram_spec.rb
@@ -0,0 +1,10 @@
+require('rspec')
+require('pry')
+require('anagram')
+
+describe("string_check") do
+  it("will return true if the user input is a string") do
+    set1 = Anagram.new(6)
+    expect(set1.user_input).to be_instance_of(String)
+  end
+end
