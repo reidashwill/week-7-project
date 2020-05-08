72a34b9a0c4242d1ec8448c4a5486de59689f69c
Reid Ashwill <reidashwill@gmail.com>
2020-05-08 09:32:30 -0700

    sucessfuly fail third test to determine if the anagram method will check if two strings are anagrams.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,7 @@
 class Anagram
   attr_accessor(:word1, :word2)
   
-  def initialize(word1)
+  def initialize()
     @word1 = word1
     @word2 = word2
   end
@@ -17,4 +17,12 @@ class Anagram
   def case_insensitive
     @word1.downcase!
   end
+  
+  def anagram
+    if @word1.split('').sort.join == @word2.split('').sort.join
+      true
+    else
+      false
+    end
+  end 
 end    
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,22 +4,34 @@ require('anagram')
 
 describe("initialize") do
   it("will determine if the user input is a string") do
-    set1 = Anagram.new("test")
+    set1 = Anagram.new
+    set1.word1 = "test"
     expect(set1.word1).to be_instance_of(String)
   end
 end
 
 describe("word_validator") do
   it("will determine if the user inputted string is a valid word by checking for the presence of vowels") do
-    set2 = Anagram.new("test")
+    set2 = Anagram.new
+    set2.word1 = "test"
     expect(set2.word_validator).to(eq(true))
   end
 end
 
 describe("case_insientitive") do
-  it("will determine if the case_insenitive method will treat all words the same regardless of case") do
-    set3 = Anagram.new("Test")
+  it("will determine if the case_insenitive method will change all user input to lowercase") do
+    set3 = Anagram.new
+    set3.word1 = "test"
     set3.case_insensitive
     expect(set3.word1).to(eq("test"))
+  end
+end
+
+describe("anagram") do
+  it("will determine if the .anagram method will compare two user inputs and return true if they are anagrams") do
+    set4 = Anagram.new
+    set4.word1 = "listen"
+    set4.word2 = "hey"
+    expect(set4.anagram).to(eq(true))
   end
 end    
